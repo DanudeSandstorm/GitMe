0f3dc52a67ddbcf19f4a918e9249ad713b3b17ed
Daniel <dds2130@rit.edu>
2016-10-22 22:38:11 -0400

    removed unused code

--- app.js
+++ app.js
@@ -4,15 +4,5 @@ var express = require('express'),
 
 app.use(express.static(__dirname));
 
-// app.use(express.static(__dirname + '/lib'));
-// app.use(express.static(__dirname + '/partials'));
-// app.use(express.static(__dirname + '/resources'));
-// app.use(express.static(__dirname + '/html'));
-
-// router.get('/', function(req, res) {
-// 	console.log('res');
-//   res.sendfile(__dirname + '/index.html');
-// });
-
 console.log("app starting on port 3000");
 app.listen(3000);
