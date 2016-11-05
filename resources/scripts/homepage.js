f396f6580eb845eac7708a92260f97dc57cf29e1
sjl9313 <sjl9313@g.rit.edu>
2016-11-05 12:55:12 -0400

    added homepage
    need to figure out a better solution for the buttons but for now you can
    click them and see an accept/reject. javascript also needs to be fixed

---
+++ resources/scripts/homepage.js
@@ -0,0 +1,9 @@
+
+
+	function myFunction()
+{
+	var myBtn = document.getElementById("button pullRequest");
+	myBtn.style.visibility = 'hidden';
+	document.getElementById("accept").style.visibility = 'visible';
+}
+
