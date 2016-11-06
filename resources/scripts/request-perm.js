6c91d9bdff3241c835fafddd2e0d089b2254aa46
kguo0427 <kxg1158@g.rit.edu>
2016-11-06 11:05:27 -0500

    1. added the pictures to the user-info page 2. combined Tim's profile page with the user-info page. So the repo buttons are clickable and the tabs are working. (Technically we don't need the profile.html page any more, but we can just leave it there to show that we have a lot of pages.) 3. made the permission request page buttons consistent with Steven's buttons on the homepage.

--- resources/scripts/request-perm.js
+++ resources/scripts/request-perm.js
@@ -1,65 +1,65 @@
 function toggle1A() {
-    var a = document.getElementById('firstLineA');
-    var b = document.getElementById('firstLineR');
+    var a = document.getElementById('accept1');
+    var b = document.getElementById('reject1');
     var r = document.getElementById('l1b');
     var t = document.getElementById('l1at');
     a.style.display = 'none';
     b.style.display = 'none';
-    r.style.backgroundColor = '#00ff00'
+    r.style.backgroundColor = '#4CAF50'
     t.className = "show";
 }
 
 function toggle1R() {
-    var a = document.getElementById('firstLineA');
-    var b = document.getElementById('firstLineR');
+    var a = document.getElementById('accept1');
+    var b = document.getElementById('reject1');
     var r = document.getElementById('l1b');
     var t = document.getElementById('l1rt');
     a.style.display = 'none';
     b.style.display = 'none';
-    r.style.backgroundColor = '#ff0000'
+    r.style.backgroundColor = '#f44336'
     t.className = "show";
 }
 
 function toggle2A() {
-    var a = document.getElementById('secondLineA');
-    var b = document.getElementById('secondLineR');
+    var a = document.getElementById('accept2');
+    var b = document.getElementById('reject2');
     var r = document.getElementById('l2b');
     var t = document.getElementById('l2at');
     a.style.display = 'none';
     b.style.display = 'none';
-    r.style.backgroundColor = '#00ff00'
+    r.style.backgroundColor = '#4CAF50'
     t.className = "show";
 }
 
 function toggle2R() {
-    var a = document.getElementById('secondLineA');
-    var b = document.getElementById('secondLineR');
+    var a = document.getElementById('accept2');
+    var b = document.getElementById('reject2');
     var r = document.getElementById('l2b');
     var t = document.getElementById('l2rt');
     a.style.display = 'none';
     b.style.display = 'none';
-    r.style.backgroundColor = '#ff0000'
+    r.style.backgroundColor = '#f44336'
     t.className = "show";
 }
 
 function toggle3A() {
-    var a = document.getElementById('thirdLineA');
-    var b = document.getElementById('thirdLineR');
+    var a = document.getElementById('accept3');
+    var b = document.getElementById('reject3');
     var r = document.getElementById('l3b');
     var t = document.getElementById('l3at');
     a.style.display = 'none';
     b.style.display = 'none';
-    r.style.backgroundColor = '#00ff00'
+    r.style.backgroundColor = '#4CAF50'
     t.className = "show";
 }
 
 function toggle3R() {
-    var a = document.getElementById('thirdLineA');
-    var b = document.getElementById('thirdLineR');
+    var a = document.getElementById('accept3');
+    var b = document.getElementById('reject3');
     var r = document.getElementById('l3b');
     var t = document.getElementById('l3rt');
     a.style.display = 'none';
     b.style.display = 'none';
-    r.style.backgroundColor = '#ff0000'
+    r.style.backgroundColor = '#f44336'
     t.className = "show";
 }
