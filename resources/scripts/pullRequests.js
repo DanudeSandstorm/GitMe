d15f471adec153c5a2d075678ee68c37b62c7639
sjl9313 <sjl9313@g.rit.edu>
2016-12-03 13:51:09 -0500

    added pages for 2 other PRs

--- resources/scripts/pullRequests.js
+++ resources/scripts/pullRequests.js
@@ -1,3 +1,12 @@
 document.getElementById("btn1").onclick = function () {
         location.href = "individualPR.html";
 };
+
+document.getElementById("btn2").onclick = function () {
+        location.href = "individualPR2.html";
+};
+
+document.getElementById("btn3").onclick = function () {
+        location.href = "individualPR3.html";
+};
+
